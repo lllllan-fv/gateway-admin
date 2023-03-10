e84012d0a4a8c82f14aaff7ad8aed2149ce6c7b3
苏桐渤 <342310798@qq.com>
2023-03-10 17:28:14 +0800

    tcp server

--- internal/proxy/server.go
+++ internal/proxy/server.go
@@ -5,9 +5,11 @@ import "github.com/lllllan-fv/gateway-admin/internal/proxy/server"
 func Run() {
 	server.HttpServerRun()
 	server.HttpsServerRun()
+	server.TcpServerRun()
 }
 
 func Stop() {
 	server.HttpServerStop()
 	server.HttpsServerStop()
+	server.TcpServerStop()
 }
